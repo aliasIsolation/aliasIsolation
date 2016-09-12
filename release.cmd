f91f3e0ce86724d103b99614112d55d0353a995e
Alan Smithee <aliasIsolation@gmail.com>
2016-09-12 20:58:49 +0200

    release script update

--- release.cmd
+++ release.cmd
@@ -6,6 +6,6 @@ xcopy /Y t2-output\win32-msvc-release-default\aliasIsolationInjector.exe release
 xcopy /Y t2-output\win32-msvc-release-default\aliasIsolationInjectorGui.exe release
 mkdir release\data
 xcopy /Y /S /E data release\data
-xcopy /Y README.md release
+xcopy /Y README.txt release
 echo aliasIsolationInjector detach > release\detachAll.cmd
 pause
